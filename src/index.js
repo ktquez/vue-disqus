6dd63d3d00320394357da4dcdceb138297657e1f
Alan Albuquerque (ktquez) <ktquez@gmail.com>
2018-04-20 19:08:57 -0300

    Add auto install component

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import VueDisqus from "./vue-disqus"
 export default function install (Vue) {
   Vue.component('VueDisqus', VueDisqus)
 }
+
+// auto install
+if (typeof Vue !== 'undefined') {
+  Vue.use(install)
+}
