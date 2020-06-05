3f28bf9fd78dc3dca663fdbdf4d83510db1e0e9d
Alan Ktquez <ktquez@gmail.com>
2020-06-05 15:37:38 -0300

    Remove livereload and serve rollup plugins

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -2,8 +2,6 @@ import buble from '@rollup/plugin-buble'
 import resolve from '@rollup/plugin-node-resolve'
 import chokidar from 'chokidar'
 import { eslint } from 'rollup-plugin-eslint'
-import livereload from 'rollup-plugin-livereload'
-import serve from 'rollup-plugin-serve'
 import vue from 'rollup-plugin-vue'
 
 export default {
@@ -21,21 +19,12 @@ export default {
       css: true,
       compileTemplate: true
     }),
-    buble(),
-    serve({
-      port: 5000,
-      verbose: true,
-      contentBase: 'example',
-      historyApiFallback: true
-    }),
-    livereload({
-      watch: 'example'
-    })
+    buble()
   ],
   output: [
     {
       name: 'VueDisqus',
-      file: 'example/vue-disqus.js',
+      file: 'demo/vue-disqus.js',
       format: 'umd',
       exports: 'named'
     }
