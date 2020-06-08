2f1e640ade361372c99bf99584f8435fc74dac7b
Alan Ktquez <ktquez@gmail.com>
2020-06-08 08:17:15 -0300

    docs: Fix base config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,10 @@
 module.exports = {
+  base: '/vue-disqus/',
   theme: 'default-vue-a11y',
   title: 'Vue Disqus',
   description: 'Vue component to integrate Disqus comments in your application, with support for SPA',
   themeConfig: {
     home: false,
-    base: '/vue-disqus/',
     repo: 'vue-a11y/eslint-plugin-vuejs-accessibility',
     docsDir: 'docs',
     docsBranch: 'master',
