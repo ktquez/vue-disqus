0aaac87da2c7b29dc0120c69ac0051571a3dff6d
Alan Ktquez <ktquez@gmail.com>
2021-04-08 08:16:59 -0300

    Update types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,42 @@
-export interface VueDisqusOptions {
-  shortname?: string
-}
+import { CALLBACKS, PAGE_CONFIG_KEYS, SSO_KEYS } from './constants';
 
-export interface VueDisqusInstance {
-  shortname: string | null,
-  reset: any
+type SSOKeys = typeof SSO_KEYS
+type PageConfigKeys = typeof PAGE_CONFIG_KEYS
+interface DisqusConfigCallbacks {
+ [key: string]: any;
+  preData?: (...args: any[]) => any;
+  preInit?: (...args: any[]) => any;
+  onInit?: (...args: any[]) => any;
+  onReady?: (...args: any[]) => any;
+  afterRender?: (...args: any[]) => any;
+  preReset?: (...args: any[]) => any;
+  onIdentify?: (...args: any[]) => any;
+  beforeComment?: (...args: any[]) => any;
+  onNewComment?: (...args: any[]) => any;
+  onPaginate?: (...args: any[]) => any;
+};
+export interface VueDisqusOptions {
+  shortname?: string;
+};
+
+export interface VueDisqusInstance extends VueDisqusOptions {
+  reset: any;
+};
+
+export interface DisqusConfig {
+  url?: string;
+  identifier?: string;
+  title?: string;
+  categoryID?: string;
+  language?: string;
+  sso?: SSOKeys;
+  page?: PageConfigKeys;
+  callbacks: DisqusConfigCallbacks;
+};
+
+declare global {
+    interface Window {
+        DISQUS: any;
+        location: Location
+    }
 }
