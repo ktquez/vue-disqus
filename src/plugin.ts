4aa40ee9b0bff2d3d74ab01f24a015fcf32db264
Alan Ktquez <ktquez@gmail.com>
2021-04-08 08:18:01 -0300

    Provide/inject disqus options

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,4 +1,5 @@
 import { App } from 'vue'
+import { DISQUS_CONFIG_KEY } from './constants'
 import { VueDisqusOptions, VueDisqusInstance } from './types';
 import Disqus from './Disqus.vue'
 import DisqusCount from './DisqusCount.vue'
@@ -12,5 +13,5 @@ export default function install (app: App, options: VueDisqusOptions = {}) {
     reset: null
   }
 
-  app.config.globalProperties.$disqus = disqusConfig
+  app.provide(DISQUS_CONFIG_KEY, disqusConfig)
 }
